4cb08b26638a6f29370f51a98d4ee64ed32d8c18
redengin <sholstei@yahoo.com>
2025-05-05 22:20:09 -0400

    in work

--- imu/imu-traits/src/lib.rs
+++ imu/imu-traits/src/lib.rs
@@ -1,50 +1,66 @@
+#![no_std]
+#[cfg(feature="std")]
+    extern crate std;
+
+// FIXME don't alias these (it's not necessary)
+#[cfg(feature="std")]
 use std::error::Error as StdError;
+#[cfg(feature="std")]
 use std::fmt;
+#[cfg(feature="std")]
 use std::io;
+#[cfg(feature="std")]
 use std::sync::mpsc;
+
+#[cfg(feature="high_accuracy")]
+type Float = f64;
+#[cfg(not(feature="high_accuracy"))]
+type Float = f32;
+
 
 // --- Basic Types ---
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct Vector3 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    pub x: Float,
+    pub y: Float,
+    pub z: Float,
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct Quaternion {
-    pub w: f32,
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    pub w: Float,
+    pub x: Float,
+    pub y: Float,
+    pub z: Float,
 }
 
 impl Vector3 {
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
+    pub fn new(x: Float, y: Float, z: Float) -> Self {
         Self { x, y, z }
     }
 
-    pub fn euler_to_quaternion(&self) -> Quaternion {
-        // Convert Euler angles (in radians) to quaternion
-        // Using the ZYX rotation order (yaw, pitch, roll)
-        let (roll, pitch, yaw) = (self.x, self.y, self.z);
+    // pub fn euler_to_quaternion(&self) -> Quaternion {
+    //     // Convert Euler angles (in radians) to quaternion
+    //     // Using the ZYX rotation order (yaw, pitch, roll)
+    //     let (roll, pitch, yaw) = (self.x, self.y, self.z);
 
-        let cr = (roll * 0.5).cos();
-        let sr = (roll * 0.5).sin();
-        let cp = (pitch * 0.5).cos();
-        let sp = (pitch * 0.5).sin();
-        let cy = (yaw * 0.5).cos();
-        let sy = (yaw * 0.5).sin();
+    //     let cr = (roll * 0.5).cos();
+    //     let sr = (roll * 0.5).sin();
+    //     let cp = (pitch * 0.5).cos();
+    //     let sp = (pitch * 0.5).sin();
+    //     let cy = (yaw * 0.5).cos();
+    //     let sy = (yaw * 0.5).sin();
 
-        let w = cr * cp * cy + sr * sp * sy;
-        let x = sr * cp * cy - cr * sp * sy;
-        let y = cr * sp * cy + sr * cp * sy;
-        let z = cr * cp * sy - sr * sp * cy;
+    //     let w = cr * cp * cy + sr * sp * sy;
+    //     let x = sr * cp * cy - cr * sp * sy;
+    //     let y = cr * sp * cy + sr * cp * sy;
+    //     let z = cr * cp * sy - sr * sp * cy;
 
-        Quaternion { w, x, y, z }
-    }
+    //     Quaternion { w, x, y, z }
+    // }
 }
 
+#[cfg(feature="std")]
 impl fmt::Display for Vector3 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Vector3(x={}, y={}, z={})", self.x, self.y, self.z)
@@ -79,6 +95,7 @@ impl Quaternion {
     }
 }
 
+#[cfg(feature="std")]
 impl fmt::Display for Quaternion {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -115,26 +132,27 @@ pub struct ImuData {
 // --- Standard Error Type ---
 #[derive(Debug)]
 pub enum ImuError {
-    /// Error originating from the underlying device communication (I2C, Serial, CAN)
-    DeviceError(String),
-    /// Error reading data from the device or internal state
-    ReadError(String),
-    /// Error writing commands or configuration to the device
-    WriteError(String),
-    /// Error during device configuration or setup
-    ConfigurationError(String),
-    /// Error related to multithreading locks (e.g., poisoned)
-    LockError(String),
-    /// Error sending a command to the reader thread
-    CommandSendError(String),
-    /// Functionality not supported by this specific IMU implementation
-    NotSupported(String),
-    /// Invalid packet received from the device
-    InvalidPacket(String),
-    /// Catch-all for other errors
-    Other(String),
+    // /// Error originating from the underlying device communication (I2C, Serial, CAN)
+    // DeviceError(String),
+    // /// Error reading data from the device or internal state
+    // ReadError(String),
+    // /// Error writing commands or configuration to the device
+    // WriteError(String),
+    // /// Error during device configuration or setup
+    // ConfigurationError(String),
+    // /// Error related to multithreading locks (e.g., poisoned)
+    // LockError(String),
+    // /// Error sending a command to the reader thread
+    // CommandSendError(String),
+    // /// Functionality not supported by this specific IMU implementation
+    // NotSupported(String),
+    // /// Invalid packet received from the device
+    // InvalidPacket(String),
+    // /// Catch-all for other errors
+    // Other(String),
 }
 
+#[cfg(feature="std")]
 impl fmt::Display for ImuError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -151,38 +169,38 @@ impl fmt::Display for ImuError {
     }
 }
 
-impl StdError for ImuError {}
+// impl StdError for ImuError {}
 
-// Add From implementations for common error types
-impl From<io::Error> for ImuError {
-    fn from(err: io::Error) -> Self {
-        ImuError::DeviceError(err.to_string())
-    }
-}
+// // Add From implementations for common error types
+// impl From<io::Error> for ImuError {
+//     fn from(err: io::Error) -> Self {
+//         ImuError::DeviceError(err.to_string())
+//     }
+// }
 
-impl From<serialport::Error> for ImuError {
-    fn from(err: serialport::Error) -> Self {
-        ImuError::DeviceError(err.to_string())
-    }
-}
+// impl From<serialport::Error> for ImuError {
+//     fn from(err: serialport::Error) -> Self {
+//         ImuError::DeviceError(err.to_string())
+//     }
+// }
 
-impl<T> From<std::sync::PoisonError<T>> for ImuError {
-    fn from(err: std::sync::PoisonError<T>) -> Self {
-        ImuError::LockError(err.to_string())
-    }
-}
+// impl<T> From<std::sync::PoisonError<T>> for ImuError {
+//     fn from(err: std::sync::PoisonError<T>) -> Self {
+//         ImuError::LockError(err.to_string())
+//     }
+// }
 
-impl<T> From<mpsc::SendError<T>> for ImuError {
-    fn from(err: mpsc::SendError<T>) -> Self {
-        ImuError::CommandSendError(err.to_string())
-    }
-}
+// impl<T> From<mpsc::SendError<T>> for ImuError {
+//     fn from(err: mpsc::SendError<T>) -> Self {
+//         ImuError::CommandSendError(err.to_string())
+//     }
+// }
 
-impl From<mpsc::RecvError> for ImuError {
-    fn from(err: mpsc::RecvError) -> Self {
-        ImuError::CommandSendError(err.to_string())
-    }
-}
+// impl From<mpsc::RecvError> for ImuError {
+//     fn from(err: mpsc::RecvError) -> Self {
+//         ImuError::CommandSendError(err.to_string())
+//     }
+// }
 
 pub trait ImuReader {
     /// Retrieves the latest available IMU data.
